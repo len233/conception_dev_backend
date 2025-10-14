dae4547fd48eea8becd8d6ff81a8249b0d233c51
小施 <mihu@WANGdeMacBook-Air-2.local>
2025-10-14 14:19:52 +0200

    node 2.13/1

--- express_server.js
+++ express_server.js
@@ -20,10 +20,10 @@ app.get('/transaction',function (req, res){
   res.json({ tableau: [100, 2000, 3000] })
 })
 
+app.get('/exo-query-string', (req, res) => {
+  res.send('hello')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-// j'ai fait une modif dans le get de la route / en ajoutant un console log pour afficher les headers et le body de la requete
-
-// le headers il affiche plein d'informations sur la requete et le body il est vide car dans une requete get on envoie pas de body donc il affiche undefined
